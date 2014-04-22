34e55971f0f2d3785438fad8df1e7c80ded8a9e5
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix sets a vector of functions.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <-NULL
+  
+  #Function 1. Overwrite in cache a matrix
+  set <- function(y) {
+    x <<- y
+  # Every time this function is called erases the inverse
+  # matrix 
+    inv <<- NULL
+  }
+  
+  ##Function 2. Retrieve original matrix
+  get <- function() x
+  
+  ##Function 3. Save in cache the inverse matrix
+  setinverse <- function(inverseMatrix) inv <<- inverseMatrix
+  
+  ##Function 4. Retrieve inverse matrix
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function just returns the inverse matrix.
+# It requires the "function vectors" created with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  inv <- x$getinverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached matrix")
+    return(inv)
+  }
+  
+  ## if there is no "inv" variable in cache,
+  ## the function continues and the inverse is calculated
+  
+  mat <- x$get()
+  message("calculating inverse matrix")
+  inv <- solve(mat, ...)
+  
+  x$setinverse(inv)
+  inv
+
 }
